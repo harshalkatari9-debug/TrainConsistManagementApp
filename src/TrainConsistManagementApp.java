c5993e35eecec823246d7d0ad903a7b46fd0b623
harshalkatari9-debug <harshalkatari9@gmail.com>
2026-04-02 14:36:23 +0530

    UC5: Maintain Ordered Unique Bogies using LinkedHashSet

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
 
 public class TrainConsistManagementApp {
 
@@ -6,23 +6,15 @@ public class TrainConsistManagementApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        LinkedList<String> trainConsist = new LinkedList<>();
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
-        System.out.println("Initial Consist: " + trainConsist);
+        trainFormation.add("Sleeper");
 
-        trainConsist.add(2, "Pantry Car");
-
-        System.out.println("After adding Pantry Car: " + trainConsist);
-
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        System.out.println("Final Train Consist: " + trainConsist);
+        System.out.println("Final Train Formation: " + trainFormation);
     }
 }
